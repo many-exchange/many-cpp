01545a98d3f2bef1f24e6bafcb2e22a4bc032e89
Partially Sorted <partiallysorted@gmail.com>
2022-12-30 21:13:17 -0600

    Fixed PDAs.

--- tests/reference/getAssociatedTokenAddress.ts
+++ tests/reference/getAssociatedTokenAddress.ts
@@ -16,30 +16,3 @@ import assert from "assert";
   console.log("Associated token address: ", associatedTokenAddress.toBase58());
 
 })();
-
-
-// (async () => {
-
-//   let publicKey = new PublicKey("7shNxW9FhomrTpYVzFuUFqxCtGrY7t4g6AmeU2TAupif");
-//   console.log(`${publicKey.toBase58()} = ${PublicKey.isOnCurve(publicKey.toBuffer())}`);
-
-//   publicKey = new PublicKey("GhWpf4AyL2NWNdSuaCDDyt2csTs6HzgpgFXrRNbY6C1B");
-//   console.log(`${publicKey.toBase58()} = ${PublicKey.isOnCurve(publicKey.toBuffer())}`);
-
-//   publicKey = new PublicKey("FPmgj6Ty6LQCv65ejJdfB3w5SgChxbwHjUvPqW3FQFUK");
-//   console.log(`${publicKey.toBase58()} = ${PublicKey.isOnCurve(publicKey.toBuffer())}`);
-
-//   publicKey = new PublicKey("2eQzsJbQnDYzRvNqwoBJeXfBeWsrbNfa1pXdNbe1JBS8");
-//   console.log(`${publicKey.toBase58()} = ${PublicKey.isOnCurve(publicKey.toBuffer())}`);
-
-//   publicKey = new PublicKey("5MdBSu6D5CNrocD4sETfsFfDrQ4HjyahaWVsp5D1eJK2");
-//   console.log(`${publicKey.toBase58()} = ${PublicKey.isOnCurve(publicKey.toBuffer())}`);
-
-//   publicKey = new PublicKey("HsF8NnhYGXFBC5bfNdD1TYfJUj8fCU72qgxLQvhcaFYj");
-//   console.log(`${publicKey.toBase58()} = ${PublicKey.isOnCurve(publicKey.toBuffer())}`);
-
-//   publicKey = new PublicKey("ApMFHdM1fohWjMpYEWWRsbxGJDFL3FqA6oe9GEGa8dbQ");
-//   console.log(`${publicKey.toBase58()} = ${PublicKey.isOnCurve(publicKey.toBuffer())}`);
-
-// })();
-
